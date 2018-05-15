243e3eb0d6dab0b8af3548cf5b77b5a70ec51849
RedChiken <gombabo1217@gmail.com>
2018-05-15 17:39:30 +0900

    addMemberToGroup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,13 +9,13 @@ const db = admin.firestore();
 exports.createUser = functions.auth.user().onCreate((userRecord, context) => {
     //Create users/userID
     return db.collection('users').doc(userRecord.uid).set({
-        email : userRecord.email
+        email: userRecord.email
     }).then(() => {
         console.log('User firebase is added at ${res.updateTime}')
     });
 });
 
-function deleteCollection(collectionPath, batchSize){
+function deleteCollection(collectionPath, batchSize) {
     const collectionRef = db.collection(collectionPath);
     const query = collectionRef.orderBy('__name__').limit(batchSize);
 
@@ -24,7 +24,7 @@ function deleteCollection(collectionPath, batchSize){
     });
 }
 
-function deleteCollection_nested(collectionPath, batchSize){
+function deleteCollection_nested(collectionPath, batchSize) {
     const collectionRef = db.collection(collectionPath);
     const query = collectionRef.orderBy('__name__').limit(batchSize);
 
@@ -33,65 +33,61 @@ function deleteCollection_nested(collectionPath, batchSize){
     });
 }
 
-function deleteQueryBatch(query, batchSize, resolve, reject){
-    query.get().then((snapshot)=>{
-        if(snapshot.size === 0){
-            return new Promise(function(){
-                console.log('All documents deleted.');
-            });
+function deleteQueryBatch(query, batchSize, resolve, reject) {
+    query.get().then((snapshot) => {
+        if (snapshot.size === 0) {
+            return 0;
         }
 
         const batch = db.batch();
-        snapshot.docs.forEach((doc)=>{
+        snapshot.docs.forEach((doc) => {
             batch.delete(doc.ref);
         });
 
-        return batch.commit().then(()=>{
+        return batch.commit().then(() => {
             return snapshot.size;
         });
-    }).then((numDeleted)=>{
-        if(numDeleted === 0){
+    }).then((numDeleted) => {
+        if (numDeleted === 0) {
             resolve();
             return;
         }
-        process.nextTick(()=>{
+        process.nextTick(() => {
             deleteQueryBatch(query, batchSize, resolve, reject);
         });
     }).catch(reject);
 }
 
-function deleteQueryBatchAndDocs(query, batchSize, resolve, reject){
-    query.get().then((snapshot)=>{
-        if(snapshot.size === 0){
-            return new Promise(function(){
-                console.log('All documents deleted.');
-            });
+function deleteQueryBatchAndDocs(query, batchSize, resolve, reject) {
+    query.get().then((snapshot) => {
+        if (snapshot.size === 0) {
+            return 0;
         }
 
         const batch = db.batch();
-        snapshot.docs.forEach((doc)=>{
+        snapshot.docs.forEach((doc) => {
             const group_id = doc.ref.parent.parent.id;
             batch.delete(doc.get('user_ref').collection('participated_group').doc(group_id));
             batch.delete(doc.ref);
         });
 
-        return batch.commit().then(()=>{
+        return batch.commit().then(() => {
             return snapshot.size;
         });
-    }).then((numDeleted)=>{
-        if(numDeleted === 0){
+    }).then((numDeleted) => {
+        if (numDeleted === 0) {
             resolve();
             return;
         }
-        process.nextTick(()=>{
-            deleteQueryBatch(query, batchSize, resolve, reject);
+        process.nextTick(() => {
+            deleteQueryBatchAndDocs(query, batchSize, resolve, reject);
         });
     }).catch(reject);
 }
 
-exports.deleteUser = functions.auth.user().onDelete((userRecord, context)=> {
-   //delete users/userID
-    db.collection('users').doc(userRecord.uid).delete().then(() => {
+exports.deleteUser = functions.auth.user().onDelete((userRecord, context) => {
+    //delete users/userID
+    return db.collection('users').doc(userRecord.uid).delete().then(() => {
         return deleteCollection('users/' + userRecord.uid + '/participated_group/', 100);
     });
 });
@@ -99,22 +95,15 @@ exports.deleteUser = functions.auth.user().onDelete((userRecord, context)=> {
 exports.deleteAllFromGroup = functions.firestore
     .document('groups/{groupID}')
     .onDelete((snap, context) => {
-        return Promise.all([db.doc(snap.owner_ref).collection('participated_group').doc(context.params.groupID).delete(),
-                                    deleteCollection_nested('groups/' + context.params.groupID + '/admin_ref/', 100),
-                                    deleteCollection_nested('groups/' + context.params.groupID + '/member_ref/', 100),
-                                    deleteCollection('groups/' + context.params.groupID + '/cabinet_ref/', 100)]).then(function(){
-                                        console.log('delete all lower documents and nested information.');
+        return Promise.all([
+            db.doc(snap.get("owner_ref")).collection('participated_group').doc(context.params.groupID).delete(),
+            deleteCollection_nested('groups/' + context.params.groupID + '/admin_ref/', 100),
+            deleteCollection_nested('groups/' + context.params.groupID + '/member_ref/', 100),
+            deleteCollection('groups/' + context.params.groupID + '/cabinet_ref/', 100)
+        ]).then(function () {
+            console.log('delete all lower documents and nested information.');
         });
         //TODO: delete All lower collections in this document.
-    });
-
-exports.deleteParticipatedGroupInOwner = functions.firestore
-    .document('groups/{groupID}')
-    .onDelete((snap, context) => {
-        //owner_ref의 participated_group에서 해당 group 삭제
-        return db.collection('users').doc(snap.data().owner_ref).collection('participated_group').doc(context.params.groupID).delete().then(() => {
-            console.log('Participated Group is deleted from Owner');
-        });
     });
 
 exports.deleteGroupInCabinet = functions.firestore
@@ -122,34 +111,76 @@ exports.deleteGroupInCabinet = functions.firestore
     .onDelete((snap, context) => {
         //cabinet_ref에 속한 cabinet의 group_ref에서 그룹 정보 초기화
         return db.collection('cabinets').doc(context.params.cabinetID).update({
-            "group_ref" : null
+            "group_ref": null
         }).then(res => {
             console.log('Group is deleted from cabinet at ${res.updateTime}');
         });
     });
 
+function findUserRef(collectionPath, email) {
+    const collectionRef = db.collection(collectionPath);
+    return collectionRef.where('email', '==', email).get().then((snapshot) => {
+        if (snapshot.size === 0) {
+            throw new HttpsError('invalid-argument', 'User not exist');
+        }
+        else {
+            return snapshot.docs()[0].id;
+        }
+    });
+}
+
+function hasAuthority(docPath, userID) {
+    const userRef = db.collection('users').doc(userID);
+    const groupRef = db.doc(docPath);
+    const groupAdminRef = groupRef.collection('admin_ref');
+    return groupRef.get().then((groupDoc) => {
+        if (userRef != groupDoc.get('owner_ref')) {
+            return groupAdminRef.doc(userRef.id).get().then((adminDoc) => {
+                if (!adminDoc.exists){
+                    throw new HttpsError('invalid-argument', 'Permission denied');
+                }
+            });
+        }
+    });
+}
+
 exports.addMemberToGroup = functions.https.onCall((data, context) => {
-   return db.collection('groups').doc(data.groupID).collection('member_ref').doc(context.auth.uid).set({
-       "email" : context.auth.token.email,
-       "user_ref" : context.params.id
-   }).then(() => {
-       db.collection('users').doc(context.params.id).collection('participated_group').doc(data.groupID).set({
-           "group_name" : data.groupName,
-           "group_ref" : data.groupID
-       }).then(() => {
-           console.log('Add user as member in group and group as participated_group in user');
-       });
-   });
-   //TODO : error 뱉기
+    return hasAuthority('groups/' + data.groupID, context.params.id).then(() => {
+        return findUserRef('users/', data.email);
+    }).then((user_id) => {
+        return db.collection('groups').doc(data.groupID).collection('member_ref').doc(user_id).set({
+            "email": data.email,
+            "user_ref": user_id
+        });
+    }).then((user_id) => {
+        db.collection('users').doc(user_id).collection('participated_group').doc(data.groupID).set({
+            "group_name": data.groupName,
+            "group_ref": data.groupID
+        });
+    }).then(() => {
+        console.log('Add user as member in group and group as participated_group in user');
+    });
+    //TODO : email을 users{userID}에서 찾아서 있으면 그 userID 사용, 없으면 error 뱉기
+    //TODO : 유저가 관리자 혹은 소유자인지 확인해야 함.
+    //TODO : 파라미터 확인. context.uid 등 삭제
 });
 
 exports.addCabinetToGroup = functions.https.onCall((data, context) => {
     //cabinet가 가진 group_refo의 cabinet_refo에 해당 cabinet 추가
     return db.collection('groups').doc(data.groupID).collection('cabinet_ref').doc(data.cabinetID).set({
-        cabinet_ref : data.cabinetID,
-        description : ''
+        cabinet_ref: data.cabinetID,
+        description: ''
     }).then(() => {
         console.log('add cabinet to specific group');
-        return {cabinet_ref : data.cabintID};
+        return {cabinet_ref: data.cabintID};
     });
+    //TODO : 요청자가 해당 그룹의 owner나 admin인지 확인
+    //TODO : 사물함이 존재하는지 검사
+    //TODO : 해당 사물함의 시리얼 키가 맞는지 검사
+    //TODO : 이미 사물함이 다른 그룹에 등록되어 있는지 검사
+    //TODO : 위 경우에 어긋나면 error 뱉기
+});
+
+exports.createGroup = functions.https.onCall((datam, context) => {
+    console.log();
 });
